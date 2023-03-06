95af61c86df9136e41790fd3c937a05cc8c49fe5
Mingxun Wang <mwang87@gmail.com>
2023-03-06 11:53:24 -0800

    updating download data

---
+++ data/get_data.sh
@@ -0,0 +1,2 @@
+cd spectra
+wget --output-document=15998_nBuOH_FT_test01.mzML https://massive.ucsd.edu/ProteoSAFe/DownloadResultFile?file=f.MSV000082622/ccms_peak/Streptomyces_Roseosporus/15998_nBuOH_FT_test01.mzML&forceDownload=true
